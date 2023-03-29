c17ece24917ef372772dc1cc8c813327aad8049e
andreyplus76 <109967767+andreyplus76@users.noreply.github.com>
2023-03-29 23:20:30 +0300

    Add files via upload

---
+++ HW3.js
@@ -0,0 +1,6 @@
+let str, str1, str2
+let print = prompt('Введіть довільний рядок', str);
+let print1 = prompt('Введіть довільний рядок', str1);
+let print2 = prompt('Введіть довільний рядок', str2);
+ alert (print1 + ' ' + print + ' ' + print2)
+    

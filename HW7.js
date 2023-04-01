42e337c4720b44f3912b159b84d64ed5249c6dff
andreyplus76 <109967767+andreyplus76@users.noreply.github.com>
2023-04-01 20:08:13 +0300

    Add files via upload

---
+++ HW7.js
@@ -0,0 +1,8 @@
+let a = prompt('Введіть перше число')
+a = Number (a)
+
+let b = prompt('Введіть друге число')
+b = Number (b)
+let c = prompt('Введіть третє число')
+c = Number (c)
+   alert((a+b+c)/3)

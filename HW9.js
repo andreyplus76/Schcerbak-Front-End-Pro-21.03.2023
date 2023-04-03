c2f6bf7aecdfc8a502a1de775b85372e2fff7e6b
andreyplus76 <109967767+andreyplus76@users.noreply.github.com>
2023-04-04 00:34:34 +0300

    Add files via upload

---
+++ HW9.js
@@ -0,0 +1,13 @@
+let numOrStr = prompt('input number or string'); console.log(numOrStr) 
+
+switch (numOrStr) {
+case null:
+console.log('ви скасували')
+break;
+case '':
+console.log('Empty String')
+break;
+case isNaN( +numOrStr ):
+console.log(' number is Ba_NaN')
+}
+console.log('OK!')
